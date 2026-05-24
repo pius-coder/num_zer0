98a8246bedf01a511ddc250607b6f974ff6d58c5
Ubuntu <ubuntu@ip-172-31-85-31.ec2.internal>
2026-05-24 11:28:42 +0000

    fix: rename spa catch-all to required, remove conflicting [...rest]

--- app/[locale]/[...rest]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from 'next/navigation'
-
-/**
- * Catch-all page for unmatched routes
- * Returns 404 for any route that doesn't match existing pages
- */
-export default function CatchAllPage() {
-  notFound()
-}
